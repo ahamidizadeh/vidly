44542148aa5d05689d24083ab79d424b3b2c0c4d
Ali Hamidizadeh <ahamidizadeh@gmail.com>
2020-07-16 10:13:03 -0700

    GET list and POST new item to list are implemented

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const app = express();
-app.listen(5000, () => console.log('connected'));
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => 
-  console.log('we connected...')
-)
+app.listen(port, () => console.log(`connected to the port : ${port}`));
+app.use(express.json());
+
+
+let movieGenres = {
+    "Troy" : "Action",
+    "Star Wars" : "Science fiction",
+    "Jigsaw" : "Thriller",
+    "Home Alone" : "PG",
+    "Shrek" : "Animation"
+};
+
+app.get('/api/genres', (req, res) => {
+  console.log('we connected...');
+  res.send(movieGenres);
+});
+// add a new genre
+app.post('/api/genres', (req, res) => {
+  Object.assign(movieGenres, req.body);
+  res.status(200).send("movie has been added, thank you:)\n" + JSON.stringify(movieGenres));
+});
+// update a gemre
+app.put('/api/genres', (req, res) => {
+  
+});
