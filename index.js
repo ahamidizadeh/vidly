c841a5689ace76106106b1cc8b1f5f54627446d9
Ali Hamidizadeh <ahamidizadeh@gmail.com>
2020-07-17 06:21:28 -0700

    validating input using joy

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Joi = require('joi');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,11 +9,11 @@ app.use(express.json());
 
 
 let movieGenres = [
-    {id:1, name:"homealone", genre:"family"},
-    {id:2, name:"Troy", genre:"Action"},
-    {id:3, name:"Gladirator", genre:"Action"},
-    {id:4, name:"StarWars", genre:"Science fiction"},
-    {id:5, name:"Shrek", genre:"Animation"}
+    {name:"homealone", genre:"family"},
+    {name:"Troy", genre:"Action"},
+    {name:"Gladirator", genre:"Action"},
+    {name:"StarWars", genre:"Science fiction"},
+    {name:"Shrek", genre:"Animation"}
     ];
 // how to show the array in a more readable way??
 app.get('/api/genres', (req, res) => {
@@ -19,11 +21,11 @@ app.get('/api/genres', (req, res) => {
   res.send(movieGenres);
 });
 
-app.get('/api/genres/:id', (req, res) => {
-    const movie = movieGenres.find(m => m.id === parseInt(req.params.id));
+app.get('/api/genres/:idx', (req, res) => {
+    const movie = movieGenres[parseInt(req.params.idx)-1];
     //add 404 for movies to be updated that arent available
     if(!movie){
-        res.status(404).send('This id does not exist, try another one.')
+        return res.status(400).send('This id does not exist, try another one.')
     }
     res.send(movie);
   });
@@ -31,30 +33,60 @@ app.get('/api/genres/:id', (req, res) => {
 // Add a new genre
 app.post('/api/genres', (req, res) => {
     //sample data : (req.body = {"name" : "lionking", "genre": "animation"})
-    const movie = {
-        "id" : movieGenres.length + 1,
-        "name" : req.body.name,
-        "genre" : req.body.genre
+    const newMovie  = req.body;
+    const result = validateMovie(newMovie)
+    if(result.error){return res.status(400).send(result.error.details[0].message)
     }
-    movieGenres.push(movie);
-    res.status(200).send(JSON.stringify(movieGenres));
+    movieGenres.push(result.value);
+    
+    res.status(200).send(movieGenres);
 });
-// update a genre
+// update a genre by id
 app.put('/api/genres/:id', (req, res) => {
   //sample data: {"updatedName": "Home Alone", "genre" : ""}
   if(parseInt(req.params.id) > movieGenres.length) {
     res.status(404).send('This id does not exist, try another one')
   } 
+  const {error} = validateMovie(req.body);
+  if(error) {
+    return res.status(400).send(error.details[0].message);
+  }
   // update the movie and send update list
   movieGenres[req.params.id-1].name = req.body.updatedName;
   movieGenres[req.params.id-1].genre = req.body.updatedGenre;
-  res.send("Thanks! here is the updated list: " + JSON.stringify(movieGenres));
+  res.send(movieGenres);
 });
+//update by name
+app.put('/api/genres', (req, res) => {
+    /*using postman send a put request to 
+    localhost:3000/api/genres/?searchBy=name 
+    (name that you want to update its genre)*/
+
+    //Also {"genre" : "Romance"} in the body and change format to json on postman
+    const movieName = req.query.searchBy;
+    const movie = movieGenres.find(m => m.name === movieName)
+    const genreUpdate = req.body.genre;
+    //update
+    movie.genre = genreUpdate;
+    //show updated version
+    res.status(200).send(movie);
+
+})
 app.delete('/api/genres/:id', (req, res) => {
     if(parseInt(req.params.id) > movieGenres.length) {
         res.status(404).send('This id does not exist, try another one')
     }
     movieGenres.splice(parseInt(req.params.id)-1, 1);
+    // change the rest of the id's to match the new list correct numbers***
     res.send(movieGenres);
-    
 })
+
+// make a function to validate the input with Joi
+function validateMovie(movie) {
+  const schema = Joi.object({
+    "name" : Joi.string().min(2).required(),
+    "genre" : Joi.string().min(5).required()
+  });
+
+  return schema.validate(movie);
+}
